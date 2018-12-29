6bc223b0895ff3c0aa2e9b57e80deb854505ad8b
xerfas04 <xerfas@outlook.fr>
2018-12-29 14:10:12 +0100

    modif

--- Blink.py
+++ Blink.py
@@ -20,10 +20,10 @@ def loop():
 	while True:
 		GPIO.output(ledPin, GPIO.HIGH)  # led on
 		print ('...led on')
-		time.sleep(1)	
-		GPIO.output(ledPin, GPIO.LOW) # led off
-		print ('led off...')
-		time.sleep(1)
+		# time.sleep(1)	
+		# GPIO.output(ledPin, GPIO.LOW) # led off
+		# print ('led off...')
+		# time.sleep(1)
 
 def destroy():
 	GPIO.output(ledPin, GPIO.LOW)     # led off
