86992bf347a85faefcb1d4c210841046267d34e7
xerfas04 <xerfas@outlook.fr>
2018-12-29 14:26:57 +0100

    défaut

--- Blink.py
+++ Blink.py
@@ -19,11 +19,11 @@ def setup():
 def loop():
 	while True:
 		GPIO.output(ledPin, GPIO.HIGH)  # led on
-		print ('...led on !')
-		# time.sleep(1)	
-		# GPIO.output(ledPin, GPIO.LOW) # led off
-		# print ('led off...')
-		# time.sleep(1)
+		print ('...led on')
+		time.sleep(1)	
+		GPIO.output(ledPin, GPIO.LOW) # led off
+		print ('led off...')
+		time.sleep(1)
 
 def destroy():
 	GPIO.output(ledPin, GPIO.LOW)     # led off
