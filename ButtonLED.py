827ef6b5238e071f6f93338d08ec19dbfe44714c
xerfas04 <xerfas@outlook.fr>
2018-12-29 15:00:59 +0100

    normal again

--- ButtonLED.py
+++ ButtonLED.py
@@ -18,12 +18,12 @@ def setup():
 
 def loop():
 	while True:
-		if GPIO.input(buttonPin)==GPIO.HIGH:
-			GPIO.output(ledPin,GPIO.LOW)
-			print ('led off ...')
-		else :
+		if GPIO.input(buttonPin)==GPIO.LOW:
 			GPIO.output(ledPin,GPIO.HIGH)
-			print ('led on ...')		
+			print ('led on ...')
+		else :
+			GPIO.output(ledPin,GPIO.LOW)
+			print ('led off ...')		
 
 def destroy():
 	GPIO.output(ledPin, GPIO.LOW)     # led off
